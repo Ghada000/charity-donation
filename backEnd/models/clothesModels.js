a7652d23f4024face3c08dcdcec852cc978dd5ac
Ghada000 <assidighada4@gmail.com>
2024-02-05 16:26:59 +0100

    clothes

--- backEnd/models/clothesModels.js
+++ backEnd/models/clothesModels.js
@@ -1,5 +1,40 @@
-const connection = require("../database/index.js");
+// const connection = require("../database/index.js");
 
+
+// module.exports = {
+//   getAll: function(callback) {
+//     const sql = 'SELECT * FROM `clothes`';
+//     connection.query(sql, function(error, results, fields) {
+//       callback(error, results);
+//     });
+//   },
+
+//   postClothes: function(postClothes, callback) {
+//     const sql = 'INSERT INTO `clothes` SET ?';
+//     connection.query(sql, postClothes, function(error, results, fields) {
+//       callback(error, results);
+//     });
+//   },
+
+//   putClothes: function(id, myData, callback) {
+//     const sql = 'UPDATE `clothes` SET ? WHERE id = ?';
+  
+//     connection.query(sql, [myData, id], function(error, results, fields) {
+//       callback(error, results);
+//     });
+//   },
+//     deleteClothes: function(id, callback) {
+//     const sql = 'DELETE FROM `clothes` WHERE id = ?';
+//     connection.query(sql, id, function(error, results, fields) {
+//       callback(error, results);
+//     });
+//   }
+// }
+
+
+
+// module.exports={}
+const connection = require("../database/index.js");
 
 module.exports = {
   getAll: function(callback) {
@@ -9,28 +44,26 @@ module.exports = {
     });
   },
 
-  postClothes: function(postClothes, callback) {
+  createClothes: function(clothesData, callback) {
     const sql = 'INSERT INTO `clothes` SET ?';
-    connection.query(sql, postClothes, function(error, results, fields) {
+    connection.query(sql, clothesData, function(error, results, fields) {
       callback(error, results);
     });
   },
 
-  putClothes: function(id, myData, callback) {
+  updateClothes: function(id, myData, callback) {
     const sql = 'UPDATE `clothes` SET ? WHERE id = ?';
-  
-    connection.query(sql, [myData, id], function(error, results, fields) {
+    const { name, image_url, season, size, gender } = myData;
+
+    connection.query(sql, [{ name, image_url, season, size, gender }, id], function(error, results, fields) {
       callback(error, results);
     });
   },
-    deleteClothes: function(id, callback) {
+
+  deleteClothes: function(id, callback) {
     const sql = 'DELETE FROM `clothes` WHERE id = ?';
     connection.query(sql, id, function(error, results, fields) {
       callback(error, results);
     });
   }
-}
-
-
-
-module.exports={}
+};
--- backEnd/server/index.js
+++ backEnd/server/index.js
@@ -1,19 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const clothesRoutes = require('../routes/clothes.js');
 
+const app = express();
 
-const route1 = require("../routes/blood.js");
-const route2 = require("../routes/clothes.js");
-const route3 = require("../routes/hair.js");
-const route3 = require("../routes/medicament.js");
-app.use("/blood", verifyToken, route1)
-app.use("/clothes", route2)
-app.use("/hair", route3)
-app.use("/medicament", route3)
+app.use(express.json());
 
+app.use('/clothes', clothesRoutes);
 
-
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
