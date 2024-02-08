7853f4f728f6d0aef4321855311353251a0bd0d3
Ghada000 <assidighada4@gmail.com>
2024-02-08 16:48:17 +0100

    ll

--- frontEnd/src/components/Medicaments.jsx
+++ frontEnd/src/components/Medicaments.jsx
@@ -1,155 +1,6 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-
-// function Medicaments() {
-//   const [data, setData] = useState([]);
-//   const [name, setName] = useState("");
-//   const [image_url, setImage_url] = useState("");
-//   const [description, setDescription] = useState("");
-//   const [category, setCategory] = useState("");
-//   const [input, setInput] = useState(false);
-
-//   const [editingId, setEditingId] = useState(null);
-
-//   useEffect(() => {
-//     axios.get('http://localhost:5000/medicaments/get')
-//       .then(function (response) {
-//         setData(response.data);
-//       })
-//       .catch(function (error) {
-//         console.log(error);
-//       });
-//   }, []);
-
-//   const handleDelete = (id) => {
-//     axios.delete(`http://localhost:5000/medicaments/${id}`)
-//       .then(() => {
-//         const updatedData = data.filter(item => item.id !== id);
-//         setData(updatedData);
-//       })
-//       .catch(error => console.log(error));
-//   }
-
-//   const handleUpdateClick = (id) => {
-//     setEditingId(id);
-//     const selectedMedicament = data.find(item => item.id === id);
-//     setName(selectedMedicament.name);
-//     setImage_url(selectedMedicament.image_url);
-//     setDescription(selectedMedicament.description);
-//     setCategory(selectedMedicament.category);
-//   }
-
-//   const handleUpdate = (id) => {
-//     axios.put(`http://localhost:5000/medicaments/${id}`, {
-//       name: name,
-//       image_url: image_url,
-//       description: description,
-//       category: category
-//     })
-//       .then(() => {
-//         const updatedData = data.map(item => {
-//           if (item.id === id) {
-//             return { ...item, name: name, image_url: image_url, description: description, category: category };
-//           }
-//           return item;
-//         });
-//         setData(updatedData);
-//         setEditingId(null);
-//         setImage_url('');
-//         setDescription('');
-//         setName('');
-//         setCategory('');
-//       })
-//       .catch(err => console.log(err));
-//   }
-
-//   const handle = () => {
-//     const newMedicament = {
-//       name: name,
-//       description: description,
-//       image_url: image_url,
-//       category: category
-//     }
-//     axios.post('http://localhost:5000/medicaments/add', newMedicament)
-//       .then(res => {
-//         // Handle success
-//         console.log(res.data);
-//         setName('');
-//         setImage_url('');
-//         setDescription('');
-//         setCategory('');
-//       })
-//       .catch(error => {
-//         // Handle error
-//         console.log(error);
-//       });
-//   }
-
-//   const isFormValid = name && image_url && description && category;
-
-//   return (
-//     <div>
-//       {data.map((item) => (
-//         <div key={item.id}>
-//           <h2>{item.category}</h2>
-//           <h2>{item.name}</h2>
-//           <h2>{item.description}</h2>
-//           <img src={item.image_url} alt={`Article ${item.id}`} />
-
-//           {editingId === item.id ? (
-//             <>
-//               <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-//               <input type="text" value={image_url} onChange={(e) => setImage_url(e.target.value)} />
-//               <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
-//               <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} />
-//               <button onClick={() => handleUpdate(item.id)}>Update</button>
-//             </>
-//           ) : (
-//             <button onClick={() => handleUpdateClick(item.id)}>Update</button>
-//           )}
-
-//           <button onClick={() => handleDelete(item.id)}>Delete</button>
-//         </div>
-//       ))}
-//       <div className="add-container">
-//         <button onClick={() => setInput(true)}>add from here</button>
-//         {input && (
-//           <div className="input-fields">
-//             <input
-//               placeholder="Name"
-//               value={name}
-//               onChange={(event) => setName(event.target.value)}
-//             />
-//             <input
-//               placeholder="ImageUrl"
-//               value={image_url}
-//               onChange={(event) => setImage_url(event.target.value)}
-//             />
-
-//             <input
-//               placeholder="description"
-//               value={description}
-//               onChange={(event) => setDescription(event.target.value)}
-//             />
-//             <input
-//               placeholder="category"
-//               value={category}
-//               onChange={(event) => setCategory(event.target.value)}
-//             />
-//           </div>
-//         )}
-//         {isFormValid && (
-//           <button onClick={handle}>You saved a life</button>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Medicaments;
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../App.css'
 
 function Medicaments() {
   const [data, setData] = useState([]);
@@ -213,7 +64,7 @@ function Medicaments() {
       .catch(err => console.log(err));
   }
 
-  const handleAdd = () => {
+  const handle = () => {
     const newMedicament = {
       name: name,
       description: description,
@@ -235,25 +86,18 @@ function Medicaments() {
       });
   }
 
-  const handleCategoryClick = (clickedCategory) => {
-    const selectedMedicament = data.find(item => item.category === clickedCategory);
-    if (selectedMedicament) {
-      setName(selectedMedicament.name);
-      setDescription(selectedMedicament.description);
-    }
-  }
-
   const isFormValid = name && image_url && description && category;
 
   return (
-    <div>
-      {data.map((item) => (
-        <div key={item.id}>
-          <h2 onClick={() => handleCategoryClick(item.category)}>{item.category}</h2>
-          <h2>{item.name}</h2>
-          <h2>{item.description}</h2>
-          <img src={item.image_url} alt={`Article ${item.id}`} />
-
+    <div className='con'>
+    {data.map((item) => (
+      <div key={item.id}>
+        <h2 className='category'>{item.category}</h2>
+        <h2 className='name'>{item.name}</h2>
+        <h2 className='description'>{item.description}</h2>
+        <img className='taswira' src={item.image_url} alt={`Article ${item.id}`} />
+        
+        <div className="button-container"> {/* Wrap the buttons in a div */}
           {editingId === item.id ? (
             <>
               <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
@@ -263,44 +107,45 @@ function Medicaments() {
               <button onClick={() => handleUpdate(item.id)}>Update</button>
             </>
           ) : (
-            <button onClick={() => handleUpdateClick(item.id)}>Update</button>
+            <button className='felsa' onClick={() => handleUpdateClick(item.id)}>Update</button>
           )}
-
-          <button onClick={() => handleDelete(item.id)}>Delete</button>
+  
+          <button className='felsa' onClick={() => handleDelete(item.id)}>Delete</button>
         </div>
-      ))}
-      <div className="add-container">
-        <button onClick={() => setInput(true)}>Add from here</button>
-        {input && (
-          <div className="input-fields">
-            <input
-              placeholder="Name"
-              value={name}
-              onChange={(event) => setName(event.target.value)}
-            />
-            <input
-              placeholder="ImageUrl"
-              value={image_url}
-              onChange={(event) => setImage_url(event.target.value)}
-            />
-
-            <input
-              placeholder="Description"
-              value={description}
-              onChange={(event) => setDescription(event.target.value)}
-            />
-            <input
-              placeholder="Category"
-              value={category}
-              onChange={(event) => setCategory(event.target.value)}
-            />
-          </div>
-        )}
-        {isFormValid && (
-          <button onClick={handleAdd}>You saved a life</button>
-        )}
       </div>
+    ))}
+    <div className="add-container">
+      <button className='felsa' onClick={() => setInput(true)}>add from here</button>
+      {input && (
+        <div className="input-fields">
+          <input
+            placeholder="Name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <input
+            placeholder="ImageUrl"
+            value={image_url}
+            onChange={(event) => setImage_url(event.target.value)}
+          />
+  
+          <input
+            placeholder="description"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          />
+          <input
+            placeholder="category"
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
+          />
+        </div>
+      )}
+      {isFormValid && (
+        <button  className='felsa' onClick={handle}>You saved a life</button>
+      )}
     </div>
+  </div>
   );
 }
 
