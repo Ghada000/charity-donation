9adfc93d3579d706c54fb7d224c48b80aafc8a6c
Roudaina-Jbeli <jbeliroudaina@gmail.com>
2024-02-10 00:44:35 +0100

    changessssssssss

--- frontEnd/src/components/EventDetails.jsx
+++ frontEnd/src/components/EventDetails.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const EventDetails = ({ event, onBackToList }) => {
   return (
     <div>
-      <h2>Event Details</h2>
-      <h3>{event.title}</h3>
-      <img src={event.image} alt="Event Image" style={{ width: '262px', height: '188px' }} />
-      <p>{event.description}</p>
-      <p>Date: {event.date}</p>
-      <p>Location: {event.location}</p>
-      <button onClick={onBackToList}>Back to List</button>
+    <h6 className='title'>Event Details</h6>
+    <div className="container d-flex justify-content-center align-items-center vh-100">
+      <div className="card">
+        <img src={event.image} className="card-img-top" alt="Event Image" style={{ height: '188px', objectFit: 'cover' }} />
+        <div className="card-body">
+          <h2 className="card-title text-center">{event.title}</h2>
+          <p className="card-text">{event.description}</p>
+          <p className="card-text"><strong>Date:</strong> {event.date}</p>
+          <p className="card-text"><strong>Location:</strong> {event.location}</p>
+          <button className="btn btn-primary d-block mx-auto" onClick={onBackToList}>Back to List</button>
+        </div>
+      </div>
+    </div>
     </div>
   );
 };
